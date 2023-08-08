ec8c3ae3785945c29fa6fad56d6db117faffff6d
Aibar <aibar.berekeyev@gmail.com>
2023-08-08 17:27:32 +0600

    s

--- app/auth/repository/repository.py
+++ app/auth/repository/repository.py
@@ -39,7 +39,7 @@ class AuthRepository:
     def get_topic_by_id(self) -> Optional[dict]:
         json = self.database["storage"].find_one(
             {
-                "_id": ObjectId("64d1d10ecd5a4554209b8438"),
+                "_id": ObjectId("64d22656cd5a455420b9c8e5"),
             }
         )
         return json
