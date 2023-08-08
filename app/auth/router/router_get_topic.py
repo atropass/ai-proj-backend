0b6a1740cbae04f073d30bf3db4d1e9a31affa22
Aibar <aibar.berekeyev@gmail.com>
2023-08-08 11:30:48 +0600

    s

--- app/auth/router/router_get_topic.py
+++ app/auth/router/router_get_topic.py
@@ -8,6 +8,7 @@ from . import router
 class GetModel(AppModel):
     phys: dict = Field(alias="Физика")
     math: dict = Field(alias="Математика")
+    chem: dict = Field(alias="Химия")
 
 
 @router.get("/json", response_model=GetModel)
@@ -15,4 +16,4 @@ def get_topic(
     svc: Service = Depends(get_service),
 ) -> dict[str, str]:
     json = svc.repository.get_topic_by_id()
-    return GetModel(phys=json["Физика"], math=json["Математика"])
+    return GetModel(phys=json["Физика"], math=json["Математика"], chem=json["Химия"])
